d3780d608b906010badd220084df5182182d1fea
porterhaus <bporter@porterhaus.com>
2016-04-06 13:15:45 -0500

    Added figaro gem.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :mentors
   devise_for :students
+  resources  :skills
+  resources  :bookmarks
+  
   get 'home/index'
   get 'home/about'
   root to: "home#index"
