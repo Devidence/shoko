a7932e0646b6256961a7fa4a74d1b5628ed0af76
Timothy Barnes <barnes7td@gmail.com>
2016-04-07 01:34:39 -0400

    US10-Added Skills index. Cleaned up routes. Added seeds for Mentors and Skills

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Mentors
+mentor = Mentor.new(
+  email: "mentor@example.com",
+  first_name: "Mentor",
+  last_name: "Smith",
+  password: "password",
+  password_confirmation: "password"
+)
+mentor.save!
+
+# Skills
+10.times do
+  Skill.create!(
+    name: Faker::Lorem.sentence(2),
+    description: Faker::Lorem.paragraph(2),
+    mentor: mentor
+  )
+end
+
+
+# Output
+# For output, just add class that you seeded to the models Array
+models = [Mentor, Skill]
+
+models.each do |model|
+  puts "#{model.count} #{model.to_s.pluralize} were created."
+end
