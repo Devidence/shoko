adac435aa5d41d5e4428a5c6e57af720f065100e
Timothy Barnes <barnes7td@gmail.com>
2016-04-07 02:33:16 -0400

    US15-Create-skill-on-skill-index

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,5 +1,24 @@
 class SkillsController < ApplicationController
   def index
+    @new_skill = Skill.new
     @skills = Skill.all
+  end
+  
+  def create
+    @skill = Skill.new(skill_params)
+    
+    # Temporary Auth Logic for Mentor
+    redirect_to :back and return unless current_mentor
+    @skill.mentor = current_mentor
+    
+    @skill.save
+    
+    redirect_to :back
+  end
+  
+  private
+  
+  def skill_params
+    params.require(:skill).permit(:name, :description)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def current_user
+    # Will need a more robust solution, at some point.
+    current_mentor || current_student
+  end
 end
