36ce7879c78217fc2c152fa505e653c3abd66f06
AnithaPal <anitha.aravindvel@gmail.com>
2016-05-05 12:02:45 -0400

    added skills show page

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -3,21 +3,26 @@ class SkillsController < ApplicationController
     @new_skill = Skill.new
     @skills = Skill.all
   end
-  
+
+  def show
+    @skill = Skill.find(params[:id])
+    @skill_bookmarks = @skill.bookmarks
+  end
+
   def create
     @skill = Skill.new(skill_params)
-    
+
     # Temporary Auth Logic for Mentor
     redirect_to :back and return unless current_mentor
     @skill.mentor = current_mentor
-    
+
     @skill.save
-    
+
     redirect_to :back
   end
-  
+
   private
-  
+
   def skill_params
     params.require(:skill).permit(:name, :description)
   end
