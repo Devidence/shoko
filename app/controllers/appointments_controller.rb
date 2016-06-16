51d735bbf69140711e6d4e823ae2825f897889a3
toyhammered <daniel@rassiner.com>
2016-06-15 20:51:59 -0700

    WIP: added some functionality to editing/deleting appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,6 +3,7 @@ class AppointmentsController < ApplicationController
   def index
     # will show all appointments based on a student
     @appointments = Appointment.where(student_id: params[:student_id])
+    @student = Student.find(params[:student_id]).first_name
   end
 
   def show
@@ -16,6 +17,22 @@ class AppointmentsController < ApplicationController
     @appointment.mentor = current_mentor
     @appointment.student = Student.find(params[:student_id])
     @appointment.save
+  end
+
+  def edit
+    @appointment = Appointment.find(params[:id])
+    @questions = Question.where(appointment: @appointment)
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @appointment = Appointment.find(params[:id])
+    @appointment.destroy
+
+    redirect_to [@appointment.student, @appointment]
   end
 
 
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,5 @@
 class Appointment < ActiveRecord::Base
   belongs_to :mentor
   belongs_to :student
-  has_many :questions
+  has_many :questions, dependent: :destroy
 end
